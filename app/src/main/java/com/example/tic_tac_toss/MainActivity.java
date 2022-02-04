62641cb469c4f1948022c56578e21cd0f978e3ba
Dorian <dorianrnd0@gmail.com>
2022-02-04 09:46:03 +0100

    ajout pop up

--- app/src/main/java/com/example/tic_tac_toss/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toss/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tic_tac_toss;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder myPopUp = new AlertDialog.Builder(this);
+        myPopUp.setTitle("Binevenue");
+        myPopUp.setMessage("Le message");
+        myPopUp.setPositiveButton("GOT IT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "C'est parti", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myPopUp.show();
+
     }
+
+
 }
