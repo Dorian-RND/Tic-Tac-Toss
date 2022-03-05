c7d197199b9ea836e7f1ad4e060370bde7c0d2d4
Hodelll <clem72.mainguy@gmail.com>
2022-03-05 13:34:26 +0100

    first try of managing the tic tac toe game

--- app/src/main/java/com/example/tic_tac_toss/TicTacToe1pEasyActivity.java
+++ app/src/main/java/com/example/tic_tac_toss/TicTacToe1pEasyActivity.java
@@ -3,6 +3,9 @@ package com.example.tic_tac_toss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TicTacToe1pEasyActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class TicTacToe1pEasyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe1p_easy);
+
+        ImageView img1 = findViewById(R.id.img_btn_1);
+
+        /*img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img1.setImageResource(R.drawable.greentest);
+            }
+        });*/
     }
 }
