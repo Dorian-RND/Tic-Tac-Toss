78cb8f592df8f7a78a24318a4605593d438f76c0
Dorian <dorianrnd0@gmail.com>
2022-03-19 17:29:37 +0100

    Player vs Player fonctionnel + ajout crédits + Ajout fond de couleur (pas beau faut revoir ça)

--- app/src/main/java/com/example/tic_tac_toss/Jeu.java
+++ app/src/main/java/com/example/tic_tac_toss/Jeu.java
@@ -1,65 +1,162 @@
 package com.example.tic_tac_toss;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.widget.ImageView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class Jeu {
 
-    CaseTicTacToe case1 = new CaseTicTacToe();
-    CaseTicTacToe case2 = new CaseTicTacToe();
-    CaseTicTacToe case3 = new CaseTicTacToe();
-    CaseTicTacToe case4 = new CaseTicTacToe();
-    CaseTicTacToe case5 = new CaseTicTacToe();
-    CaseTicTacToe case6 = new CaseTicTacToe();
-    CaseTicTacToe case7 = new CaseTicTacToe();
-    CaseTicTacToe case8 = new CaseTicTacToe();
-    CaseTicTacToe case9 = new CaseTicTacToe();
+    private Activity activity;
+    private boolean winP1;
+    private boolean winP2;
+    int tour;
+    int cpt;
 
-    ImageView btn1;
-    ImageView btn2;
-    ImageView btn3;
-    ImageView btn4;
-    ImageView btn5;
-    ImageView btn6;
-    ImageView btn7;
-    ImageView btn8;
-    ImageView btn9;
-
-    CaseTicTacToe[] case_morpion = {case1, case2, case3, case4, case5, case6, case7, case8, case9};
-    ImageView[] bouton = {btn1, btn2, btn3, btn4, btn5, btn5, btn6, btn7, btn8, btn9};
-
-    int player1 = 1;
-    int player2 = 2;
-    int tour = 0;
-
-    public Jeu(ImageView[] btn1, CaseTicTacToe[] case_tic_tac_toe) {
-        this.bouton = btn1;
-        this.case_morpion = case_tic_tac_toe;
+    public Jeu(){
+        this.winP1 = false;
+        this.winP2 = false;
+        this.tour = 1;
+        this.cpt = 0;
     }
 
-    public void validation(){
-        for(int tour=0; tour<8; tour++){
-            for(int i=0; i<8; i++){
-                if (case_morpion[i].isEmpty()){
-                    bouton[i].setImageResource(R.drawable.cross);
-                    case_morpion[i].setNotEmpty();
-                    case_morpion[i].setValeur(player1);
+    public Activity getActivity(){
+        return activity;
+    }
+
+    public void setActivity(Activity activity1){
+        this.activity = activity1;
+    }
+
+    public void CheckLigne(CaseTicTacToe[][] listCase){
+        int j = 0;
+        for(int i=0; i<3; i++){
+            if((listCase[i][j].getValeur() == 1) && (listCase[i][j+1].getValeur() == 1) && listCase[i][j+2].getValeur()==1){
+                winP1 = true;
+            }
+            if((listCase[i][j].getValeur() == 2) && (listCase[i][j+1].getValeur() == 2) && listCase[i][j+2].getValeur()==2){
+                winP2 = true;
+            }
+        }
+    }
+
+    public void CheckColonne(CaseTicTacToe[][] listCase){
+        int i = 0;
+        for(int j=0; j<3; j++){
+            if((listCase[i][j].getValeur() == 1) && (listCase[i+1][j].getValeur() == 1) && listCase[i+2][j].getValeur()==1){
+                winP1 = true;
+            }
+            if((listCase[i][j].getValeur() == 2) && (listCase[i+1][j].getValeur() == 2) && listCase[i+2][j].getValeur()==2){
+                winP2 = true;
+            }
+        }
+    }
+
+    public void CheckDiagonale(CaseTicTacToe[][] listCase){
+        int i = 0;
+        int j1 = 0;
+        int j2 = 2;
+        if(((listCase[i][j1].getValeur() == 1) && (listCase[i+1][j1+1].getValeur() == 1) && (listCase[i+2][j2].getValeur()==1)) || ((listCase[i][j2].getValeur() == 1) && (listCase[i+1][j1+1].getValeur() == 1) && (listCase[i+2][j1].getValeur()==1))){
+            winP1 = true;
+        }
+        if(((listCase[i][j1].getValeur() == 2) && (listCase[i+1][j1+1].getValeur() == 2) && (listCase[i+2][j2].getValeur()==2)) || ((listCase[i][j2].getValeur() == 2) && (listCase[i+1][j1+1].getValeur() == 2) && (listCase[i+2][j1].getValeur()==2))){
+            winP2 = true;
+        }
+    }
+
+    public boolean CheckWin(CaseTicTacToe[][] listCase){
+        CheckLigne(listCase);
+        CheckColonne(listCase);
+        CheckDiagonale(listCase);
+
+        if(winP1){
+            AlertDialog.Builder myPopup = new AlertDialog.Builder(activity);
+            myPopup.setTitle("Joueur 1 a gagné");
+            myPopup.setPositiveButton("Accueil", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent accueil = new Intent(activity.getApplicationContext(), TicTacToeActivity.class);
+                    accueil.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    activity.startActivity(accueil);
+                    activity.finish();
                 }
-                else if (case_morpion[i].isEmpty() && tour==2){
-                    bouton[i].setImageResource(R.drawable.circle);
-                    case_morpion[i].setNotEmpty();
-                    case_morpion[i].setValeur(player2);
+            }).setNegativeButton("Rejouer", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent rejouer = new Intent(activity.getApplicationContext(), TicTacToe2pActivity.class);
+                    rejouer.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    activity.startActivity(rejouer);
+                    activity.finish();
                 }
-                else {
-                    if (case_morpion[i].getValeur() == player1) {
-                        bouton[i].setImageResource(R.drawable.cross);
-                        case_morpion[i].setValeur(player1);
-                    }
-                    else {
-                        bouton[i].setImageResource(R.drawable.circle);
-                        case_morpion[i].setValeur(player2);
-                    }
+            });
+            myPopup.show();
+            return true;
+        }
+
+        else if(winP2){
+            AlertDialog.Builder myPopup = new AlertDialog.Builder(activity);
+            myPopup.setTitle("Joueur 2 a gagné");
+            myPopup.setPositiveButton("Accueil", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent accueil = new Intent(activity.getApplicationContext(), TicTacToeActivity.class);
+                    accueil.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    activity.startActivity(accueil);
+                    activity.finish();
+                }
+            }).setNegativeButton("Rejouer", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent rejouer = new Intent(activity.getApplicationContext(), TicTacToe2pActivity.class);
+                    rejouer.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    activity.startActivity(rejouer);
+                    activity.finish();
+                }
+            });
+            myPopup.show();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void CheckNull(){
+        if(cpt == 9 && (!winP1 || !winP2)){
+            AlertDialog.Builder myPopup = new AlertDialog.Builder(activity);
+            myPopup.setTitle("Aucun joueur n'a gagné");
+            myPopup.setPositiveButton("Accueil", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent accueil = new Intent(activity.getApplicationContext(), TicTacToeActivity.class);
+                    accueil.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    activity.startActivity(accueil);
+                    activity.finish();
+                }
+            }).setNegativeButton("Rejouer", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent goPage1 = new Intent(activity.getApplicationContext(), TicTacToe2pActivity.class);
+                    goPage1.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    activity.startActivity(goPage1);
+                    activity.finish();
+                }
+            });
+            myPopup.show();
+        }
+    }
+
+    public void play(CaseTicTacToe cases, ImageView btn){
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(cases.isEmpty() && tour==1){
+                    cpt++;
+                    btn.setImageResource(R.drawable.cross);
+                    cases.setNotEmpty();
+                    cases.setValeur(1);
+                    tour = 2;
+                }
+                else if(cases.isEmpty() && tour==2){
+                    cpt++;
+                    btn.setImageResource(R.drawable.circle);
+                    cases.setNotEmpty();
+                    cases.setValeur(2);
+                    tour = 1;
                 }
             }
         }
